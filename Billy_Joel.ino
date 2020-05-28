8e1dcda9047405b1496e083b28780747c60678bb
Victor Garcia <63882283+BeetorBot@users.noreply.github.com>
2020-05-27 22:39:55 -0700

    Finished

--- Billy_Joel.ino
+++ Billy_Joel.ino
@@ -9,62 +9,64 @@ int w=400;
 int h=200;
 int q=100;
 int e=500;
+int r=600;
 int bpm = 60;
 int wholeNote =2000; // w in the array
 int halfNote =wholeNote/2; // h in the array
 int quarterNote =wholeNote/4; //q in the array
 int eightNote = wholeNote/8; //o in the array
+int sixthNote = wholeNote/16;
 int flag=0;
 int StepCount;//the amounts of steps the motor takes
 int flag2=0;
 const int buttonPin = 6;
-const int tracingPin = 9;
 boolean setdir = LOW; 
-const int SPK=160;// steps per Key
+const int SPK=270;// steps per Key
 int led=10;
 int solenoidBlack= 11;
 int solenoidWhite= 12;
-int BoW;
 int buttonState = 0;
-
-//This is where you write your song. The keys go form 1 to 59 and then a note type after it to change the duration of the note. You can use my piano sketch to make this procces
-//much easier
-int Notes[]={41,e,40,e,41,e,40,e,41,e,36,e,39,e,37,e,34,e,22,e,25,e,29,e,34,e,36,q,17,e,21,e,29,e,37,e,36,e,34,q,41,e,40,e,41,e,40,e,41,e,36,e,39,e,37,e,34,e,22,e,25,e,29,e,34,e,36,q,17,e,21,e,29,e,33,e,36,e,37,q,
-41,e,40,e,41,e,40,e,41,e,36,e,39,e,37,e,34,e,22,e,25,e,29,e,34,e,36,q,17,e,21,e,29,e,37,e,36,e,34,q};
-
+//int Notes[]={1,h,2,h,3,h,14,h,25,h,16,h};
+int Notes[]={41,e,40,e,41,e,40,e,41,e,36,e,39,e,37,e,34,q,22,e,25,e,29,e,34,e,36,q,17,e,21,e,29,e,37,e,36,e,34,q,41,e,40,e,41,e,40,e,41,e,36,e,39,e,37,e,34,q,22,e,25,e,29,e,34,e,36,q,17,e,21,e,29,e,33,e,36,e,37,q,
+41,e,40,e,41,e,40,e,41,e,36,e,39,e,37,e,34,q,22,e,25,e,29,e,34,e,36,q,17,e,21,e,29,e,37,e,36,e,34,w};
 int oneNote;
 void setup() {
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT);a
    myStepper.setSpeed(stepSpeed);
      pinMode(led, OUTPUT);
 Serial.begin(9600);
   pinMode(solenoidBlack, OUTPUT);
     pinMode(solenoidWhite, OUTPUT);
-  pinMode(tracingPin, INPUT);
 }
 
 void loop() {
 while(flag==0){
-  while(flag2==0){//To start the motor at the beginning
-    buttonState = digitalRead(buttonPin);
-  myStepper.step(-SPK); 
-  if(buttonState == HIGH){
+ while(flag2==0){//To start the motor at the beginning
+   buttonState = digitalRead(buttonPin);
+   if(buttonState == HIGH){
     flag2++;
-    break;}}
-    
+    break;}
+    myStepper.setSpeed(150);
+  myStepper.step(-SPK/2); 
+  }
+    myStepper.setSpeed(stepSpeed);
 delay(1000);
 StepCount=1;
 int Size=(sizeof(Notes)/sizeof(*Notes));
+Serial.print("Size ");
 Serial.println(Size);
   for(int i=0;i<Size;i++){
-    oneNote=Notes[i];
-    SongOne(Notes[i]);
-}//Call this method when the songs go from sec 1 to 5 in order
+    oneNote=Notes[i];//track where it is
+    Serial.print("StepCount ");
+    Serial.println(StepCount);
+        SongOne(Notes[i]);
+}
 flag++;
-}}
+}
+}
 
   void SongOne(int notes){
-  if(notes==w||notes==h||notes==q||notes==e){
+  if(notes==w||notes==h||notes==q||notes==e||notes==r){
   Measures(notes);}
 
 if(notes==Keys[notes]){
@@ -74,34 +76,33 @@ aNoteStep(StepCount, Keys[notes]);}
 void aNoteStep(int SC, int key){
 
 if(SC==key){
-aNote();}
+aNote(key);}
 else if(SC>key){
-  for(int i = SC;i>=key;i--){
+  for(int i = SC;i>key;i--){
     myStepper.step(-SPK);
     StepCount--;
   }
-  aNote();
+  aNote(key);
 }
 else if(SC<key){
-  for(int i=SC;i<=key;i++){
+  for(int i=SC;i<key;i++){
     myStepper.step(SPK);
     StepCount++;
   }
-    aNote();
-}}
-
+    aNote(key);
+}
+}
 
 // plays the notes
-void aNote(){
-  int val = digitalRead(tracingPin);
+void aNote(int KEY){
   Serial.println(oneNote);//prints out the note number
-  if(val==HIGH){
+  if(KEY==2||KEY==4||KEY==7||KEY==9||KEY==11||KEY==14||KEY==16||KEY==19||KEY==21||KEY==23||
+  KEY==26||KEY==28||KEY==31||KEY==33||KEY==35||KEY==38||KEY==40||KEY==43||KEY==45||KEY==47||KEY==50||KEY==52||KEY==55||KEY==57||KEY==59){
  digitalWrite(solenoidBlack, HIGH);
- BoW=0;}
- else if(val==LOW){
+}
+ else {
  digitalWrite(solenoidWhite, HIGH);
- Serial.println(BoW);
- BoW=1;}
+}
  digitalWrite(led, HIGH);
 }
 
@@ -110,38 +111,38 @@ void Measures(int tempo){
 
 if(tempo==w){  delay(wholeNote);
   Serial.println("wholeNote");
-  if(BoW==0)
     digitalWrite(solenoidBlack, LOW);
-    else if(BoW==1)
     digitalWrite(solenoidWhite, LOW);
      digitalWrite(led, LOW);
   //lift the finger
 }
 if(tempo==h){  delay(halfNote);
   Serial.println("halfNote");
-   if(BoW==0)
     digitalWrite(solenoidBlack, LOW);
-    else if(BoW==1)
     digitalWrite(solenoidWhite, LOW);
       digitalWrite(led, LOW);
 }
 if(tempo==q){ delay(quarterNote);
   Serial.println("quarterNote");
-     if(BoW==0)
     digitalWrite(solenoidBlack, LOW);
-    else if(BoW==1)
     digitalWrite(solenoidWhite, LOW);
       digitalWrite(led, LOW);
 }
   if(tempo==e){
     delay(eightNote);
-    Serial.println(eightNote);
   Serial.println("eightNote");
-      if(BoW==0)
     digitalWrite(solenoidBlack, LOW);
-    else if(BoW==1)
     digitalWrite(solenoidWhite, LOW);
       digitalWrite(led, LOW);
 }
-delay(50);//delays the next note by a little bit to give the solenoidBlack enough time to return
+if(tempo==r){
+    delay(sixthNote);
+  Serial.println("sixthtNote");
+    digitalWrite(solenoidBlack, LOW);
+    digitalWrite(solenoidWhite, LOW);
+      digitalWrite(led, LOW);
+ 
+}
+Serial.println("delay");
+delay(100);//delays the next note by a little bit to give the solenoidBlack enough time to return
 }
